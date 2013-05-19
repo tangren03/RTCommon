a321390f6316528e77807300d8feb6f0155fb829
RyanTang <ryantang1988@gmail.com>
2013-05-19 10:44:13 +0800

    Support for Android Studio

--- src/com/ryantang/common/utils/Constant.java
+++ src/com/ryantang/common/utils/Constant.java
@@ -5,5 +5,7 @@ public interface Constant {
 	public static final int INTERNET_FAILED = -1;
 	public static final int FAILED = 0;
 	public static final int SUCCESS = 1;
+	public static String YES = "YES";
+	public static String NO = "NO";
 	
 }
